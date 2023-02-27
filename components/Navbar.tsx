eebd17d43a3a7d3e19b2f21c69d5116a8659f807
scottburack <itsmescottb123@gmail.com>
2023-02-27 18:45:18 -0500

    log in and auth state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,13 @@ import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '@/utils';
+
+import useAuthStore from '@/store/authStore';
 
 
 const Navbar = () => {
-  const user = false;
+  const { userProfile, addUser } = useAuthStore()
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -27,11 +30,11 @@ const Navbar = () => {
       <div>SEARCH</div>
 
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div>{userProfile?.userName}</div>
         ) : (
           <GoogleLogin
-            onSuccess={(response) => console.log(response)}
+            onSuccess={(response) => createOrGetUser(response, addUser)}
             onError={() => console.log('Error')}
            />
         )}
